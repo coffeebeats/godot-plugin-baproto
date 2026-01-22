efe331080a3761d31e0d92f5123ddcaf68fe2411
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2026-01-22 15:23:02 -0800

    feat(import): create an `EditorImportPlugin` for compiling schema files (#32)
    * feat(import): create editor import plugin for `.baproto` files
    
    * feat(import): create wrapper around project settings
    
    * refactor: improve clarity of implementation
    
    * fix(import): use `ProjectSetting` to simplify binary path resolution
    
    * fix: address formatting and linter errors
    
    * chore: remove unnecessary tests
    
    * chore: fix formatting (on behalf of @coffeebeats)
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- baproto.gd
+++ baproto.gd
@@ -1,16 +1,55 @@
+##
+## baproto.gd
+##
+## A Godot addon that provides an editor plugin which compiles build-a-proto schema files
+## into GDScript.
+##
+
 @tool
 extends EditorPlugin
 
-const _NAMESPACE := "BAProto"
+# -- DEFINITIONS --------------------------------------------------------------------- #
+
+const NAMESPACE := "BAProto"
+
+# -- DEPENDENCIES -------------------------------------------------------------------- #
+
+const Platform := preload("./import/platform.gd")
+const ImportPlugin := preload("./import/plugin.gd")
+const ProjectSetting := preload("./import/setting.gd")
+
+# -- INITIALIZATION ------------------------------------------------------------------ #
+
+var _import_plugin: EditorImportPlugin = null
+
+# -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
 
 
 func _enter_tree() -> void:
-	add_autoload_singleton(_NAMESPACE, "res://baproto.gd")
+	# Initialize project settings
+	ProjectSetting.binary_path()
+	ProjectSetting.output_directory()
 
-	print("[%s]: Successfully loaded addon.")
+	# Register runtime autoload
+	add_autoload_singleton(NAMESPACE, "./runtime/runtime.gd")
+
+	# Clear platform cache
+	Platform.clear_cache()
+
+	# Register import plugin
+	_import_plugin = ImportPlugin.new()
+	add_import_plugin(_import_plugin)
+
+	print("[baproto] Successfully loaded addon.")
 
 
 func _exit_tree() -> void:
-	remove_autoload_singleton(_NAMESPACE)
+	# Remove import plugin
+	if _import_plugin:
+		remove_import_plugin(_import_plugin)
+		_import_plugin = null
 
-	print("[%s]: Successfully unloaded addon.")
+	# Remove runtime autoload
+	remove_autoload_singleton(NAMESPACE)
+
+	print("[baproto] Successfully unloaded addon.")
---
+++ import/setting.gd
@@ -0,0 +1,104 @@
+##
+## import/setting.gd
+##
+## Lightweight wrapper around Godot's 'ProjectSettings' API. Provides a cleaner
+## interface for initializing, reading, and writing project settings.
+##
+
+@tool
+extends RefCounted
+
+# -- DEPENDENCIES -------------------------------------------------------------------- #
+
+const ProjectSetting := preload("./setting.gd")
+
+# -- INITIALIZATION ------------------------------------------------------------------ #
+
+var _name: String
+
+# -- PUBLIC METHODS ------------------------------------------------------------------ #
+
+
+## `binary_path` returns a new `ProjectSetting` instance for the 'baproto' binary path
+## setting.
+static func binary_path() -> ProjectSetting:
+	return ProjectSetting.new(
+		"baproto/binary/path",
+		"",
+		false,
+		TYPE_STRING,
+		PROPERTY_HINT_FILE,
+		"*.exe" if OS.get_name() == "Windows" else "*"
+	)
+
+
+## `output_directory` returns a new `ProjectSetting` instance for the output directory
+## setting.
+static func output_directory() -> ProjectSetting:
+	return ProjectSetting.new(
+		"baproto/generate/output_directory",
+		"res://baproto",
+		true,
+		TYPE_STRING,
+		PROPERTY_HINT_DIR
+	)
+
+
+## `clear` completely removes the setting from `ProjectSettings`.
+##
+## NOTE: This is the same thing as calling `set_value` with `null`.
+func clear() -> void:
+	set_value(null)
+
+
+## `get_value` retrieves the current value of the setting.
+func get_value() -> Variant:
+	var value: Variant = ProjectSettings.get_setting(_name)
+	assert(value != null, "invalid state; missing setting value")
+	return value
+
+
+## `set_value` updates the setting to the given value and saves `ProjectSettings`.
+func set_value(value: Variant) -> void:
+	ProjectSettings.set_setting(_name, value)
+	ProjectSettings.save()
+
+
+# -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
+
+
+## `_init` creates a new `ProjectSetting` and initializes it in `ProjectSettings`.
+func _init(
+	name: String,
+	default_value: Variant,
+	basic: bool = true,
+	type: int = TYPE_NIL,
+	hint: int = PROPERTY_HINT_NONE,
+	hint_string: String = ""
+) -> void:
+	_name = name
+
+	if ProjectSettings.has_setting(_name):
+		return
+
+	ProjectSettings.set_setting(_name, default_value)
+	ProjectSettings.set_initial_value(_name, default_value)
+	ProjectSettings.set_as_basic(_name, basic)
+
+	if type != TYPE_NIL:
+		var property_info := {
+			"name": _name,
+			"type": type,
+			"hint": hint,
+		}
+
+		if not hint_string.is_empty():
+			property_info["hint_string"] = hint_string
+
+		ProjectSettings.add_property_info(property_info)
+
+	ProjectSettings.save()
+
+	prints(
+		"[baproto]", "Created project setting: %s (default=%s)" % [_name, default_value]
+	)
